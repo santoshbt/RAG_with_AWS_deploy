6121ad50f42c49a47c094e057e00cbcd81bc80b5
llssbb <bt.santosh01@gmail.com>
2024-04-25 21:47:34 +0100

    code file updated

--- main.py
+++ main.py
@@ -13,6 +13,10 @@ prompt = """
 bedrock = boto3.client(service_name="bedrock-runtime")
 
 payload = {
+    "prompt": "[INST]"+prompt+"{/INST}",
+    "max_gen_len": 512,
+    "temperature": 0.3,
+    "top_p": 0.9
 
 }
 
@@ -21,11 +25,11 @@ model_id = "meta.llama2-70b-chat-v1"
 
 response = bedrock.invoke_model(
   body=body,
-  model_id=model_id,
+  modelId=model_id,
   accept="application/json",
-  content_type="application/json"
+  contentType="application/json"
 )
 
 response_body = json.loads(response.get("body").read())
-response_text = response_body("generation")
+response_text = response_body["generation"]
 print(response_text)
